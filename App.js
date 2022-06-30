19795a2267714b5c763ba4ea4dcaf95d07e81b6f
Ilso <ilso_1000@hotmail.com>
2022-06-30 10:16:01 -0300

    feat: add app.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
+
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar />
+      <Cesta {...mock} />
+    </SafeAreaView>
+  );
+}
